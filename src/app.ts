6bb5cfb4af4f933a6a9b132d18a1d19698ed59d0
Jaramill0 <hugo98jaramillo@gmail.com>
2023-02-02 18:19:28 -0600

    configurando las respuesta del entorno de TypeScript e instalando dependencias de cambios instantaneos

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,17 @@ import express, { Request, Response } from 'express';
 
 const app = express();
 
-app.get('/hello', (req: Request, res: Response) => {
+app.get('/hello', (_req: Request, res: Response) => {
     res.send({ message: "Hello World" });
 });
+const PORT = 3000
 
-app.listen(3000, () => console.log('App listening on PORT 3000'));
+app.get('/ping', (_req, res) => {
+    console.log('Someone pinged here!!' + new Date().toLocaleDateString())
+    res.send('pong')
+})
+app.use(express.json()) //middleware que trasforma la req.body a un json
+
+app.listen(PORT, () => {
+    console.log(`Server Run on port ${PORT}`);
+});
