629282011be65ae75adc128342325dae13a2c078
Jaramill0 <hugo98jaramillo@gmail.com>
2023-02-02 18:30:45 -0600

    Importando el archivo de rutas en el app

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from 'express';
-
+import diaryRouter from './routes/diaries';
 const app = express();
 
 app.get('/hello', (_req: Request, res: Response) => {
@@ -11,6 +11,8 @@ app.get('/ping', (_req, res) => {
     console.log('Someone pinged here!!' + new Date().toLocaleDateString())
     res.send('pong')
 })
+app.use('/api/diaries', diaryRouter);
+
 app.use(express.json()) //middleware que trasforma la req.body a un json
 
 app.listen(PORT, () => {
