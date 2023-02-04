20c3373ccf4838f01ede5b0234b83126b1134c80
Jaramill0 <hugo98jaramillo@gmail.com>
2023-02-04 02:22:27 -0600

    Formateo de codigo con Eslint

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,20 @@
-import express, { Request, Response } from 'express';
-import diaryRouter from './routes/diaries';
-const app = express();
+import express, { Request, Response } from 'express'
+import diaryRouter from './routes/diaries'
+const app = express()
 
 app.get('/hello', (_req: Request, res: Response) => {
-    res.send({ message: "Hello World" });
-});
+  res.send({ message: 'Hello World' })
+})
 const PORT = 3000
 
 app.get('/ping', (_req, res) => {
-    console.log('Someone pinged here!!' + new Date().toLocaleDateString())
-    res.send('pong')
+  console.log('Someone pinged here!!' + new Date().toLocaleDateString())
+  res.send('pong')
 })
-app.use('/api/diaries', diaryRouter);
+app.use('/api/diaries', diaryRouter)
 
-app.use(express.json()) //middleware que trasforma la req.body a un json
+app.use(express.json()) // middleware que trasforma la req.body a un json
 
 app.listen(PORT, () => {
-    console.log(`Server Run on port ${PORT}`);
-});
+  console.log(`Server Run on port ${PORT}`)
+})
