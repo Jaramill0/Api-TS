f047a557949d13c7b09292d120f3d7dc9dda9ebc
Jaramill0 <hugo98jaramillo@gmail.com>
2023-02-04 01:21:32 -0600

    Integrando el filtrado de la visibilidad de los datos del clima mediante un map, regresando los campos elegidos

--- src/services/diaryServices.ts
+++ src/services/diaryServices.ts
@@ -1,10 +1,14 @@
 import { DiaryEntry, NoSensitiveInfoDiaryEntry } from '../types'
 import diaryData from './diaries'
 
-const diaries: DiaryEntry[] = diaryData as DiaryEntry[]
+const diaries: DiaryEntry[] = diaryData
 
 export const getEntries = (): DiaryEntry[] => diaries
 
-export const getEntriesWithoutSensitiveInfo = (): NoSensitiveInfoDiaryEntry[] => diaries
+export const getEntriesWithoutSensitiveInfo = (): NoSensitiveInfoDiaryEntry[] => {
+  return diaries.map(({ id, date, weather, visibility }) => {
+    return { id, date, weather, visibility }
+  })
+}
 
 export const addEntry = (): undefined => undefined
