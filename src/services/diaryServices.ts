f0ec9d1322e4bfe3583b3c30f44513ac7c913a3d
Jaramill0 <hugo98jaramillo@gmail.com>
2023-02-06 21:37:44 -0600

    Add Export addDiary that weather in the method diaries

--- src/services/diaryServices.ts
+++ src/services/diaryServices.ts
@@ -1,14 +1,25 @@
-import { DiaryEntry, NoSensitiveInfoDiaryEntry } from '../types'
+import { DiaryEntry, newDiaryEntry, NoSensitiveInfoDiaryEntry } from '../types'
 import diaryData from './diaries'
 
 const diaries: DiaryEntry[] = diaryData
 
 export const getEntries = (): DiaryEntry[] => diaries
 
+export const findById = (id: number): DiaryEntry | undefined => {
+  const entry = diaries.find(d => d.id === id)
+  return entry
+}
 export const getEntriesWithoutSensitiveInfo = (): NoSensitiveInfoDiaryEntry[] => {
   return diaries.map(({ id, date, weather, visibility }) => {
     return { id, date, weather, visibility }
   })
 }
-
-export const addEntry = (): undefined => undefined
+// modificacion mas estetica de codigo y menos sobre escribir metodos existentes
+export const addDiary = (newDiaryEntry: newDiaryEntry): DiaryEntry => {
+  const newDiary = {
+    id: Math.max(...diaries.map(d => d.id)) + 1,
+    ...newDiaryEntry
+  }
+  diaries.push(newDiary)
+  return newDiary
+}
